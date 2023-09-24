856bb0ac78758f47d10bb037fc19e52ccd364c0f
Pandora Becker Lovatto <Pandora-blovatto@educar.rs.gov.br>
2023-09-24 19:37:00 -0300

    fimm

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages";
 import MovieDetails from "./pages/components/MovieDetails"; 
 import SeriesDetails from "./pages/components/SeriesDetails"; 
-import axios from 'axios';
+
 
 const App = () => {
     return ( 
